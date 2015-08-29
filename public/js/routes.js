ca0e0a5d272438ea46f40d5b121cdad29e797b30
MichaelAPace <mapace@gmail.com>
2015-08-29 13:48:42 -0600

    path error fixed

--- public/js/routes.js
+++ public/js/routes.js
@@ -2,9 +2,9 @@ app.config(function($routeProvider, $locationProvider){
 
   $routeProvider
   .when('/', {
-    templateUrl: 'partials/home.html'
+    templateUrl: 'public/partials/home.html'
   }).when('/cart', {
-    templateUrl:'partials/shoppingCart.html'
+    templateUrl:'public/partials/shoppingCart.html'
   }).otherwise({redirectTo:"/"});
 
   $locationProvider.html5Mode(true);
